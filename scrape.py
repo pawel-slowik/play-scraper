634381217518069387600bd2e576753a58b2b52e
Paweł Słowik <pawel.slowik@zoho.com>
2020-11-20 13:33:23 +0100

    Move service and balance parsing to a separate class

--- scrape.py
+++ scrape.py
@@ -48,12 +48,12 @@ class Scraper():
     def get_balance(self) -> Mapping[str, BalanceValue]:
         dwr_method = DWRBalance()
         balance_html = dwr_method.call(self.session, **{"dwr_id": self.init_dwr()})
-        return self.parse_balance_data(balance_html)
+        return Parser().parse_balance_data(balance_html)
 
     def list_services(self) -> Mapping[str, bool]:
         dwr_method = DWRServices()
         services_html = dwr_method.call(self.session, **{"dwr_id": self.init_dwr()})
-        return self.parse_services_data(services_html)
+        return Parser().parse_services_data(services_html)
 
     def log_out(self) -> None:
         response = self.session.get(self.logout_url)
@@ -70,162 +70,6 @@ class Scraper():
             domain=dwr_method.cookie_domain
         )
         return self.dwr_id
-
-    def parse_balance_data(self, html_code: str) -> Mapping[str, BalanceValue]:
-
-        def parse_balance(balance_str: str) -> float:
-            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]{2})){0,1} z\u0142", balance_str)
-            if not match:
-                raise ValueError("invalid balance: %s" % balance_str)
-            return parse_float(match)
-
-        def parse_date(date_str: str) -> datetime.date:
-            return datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
-
-        def parse_data_cap(cap_str: str) -> float:
-            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]+)){0,1} (?P<unit>GB|MB)", cap_str)
-            if not match:
-                raise ValueError("invalid data cap: %s" % cap_str)
-            value = parse_float(match)
-            if match.group("unit") == "MB":
-                value /= 1000
-            return value
-
-        def parse_hours_minutes(hours_minutes_str: str) -> int:
-            match = re.search("^(?P<hours>[0-9]+):(?P<minutes>[0-9]+) min", hours_minutes_str)
-            if not match:
-                raise ValueError("invalid hours:minutes value: %s" % hours_minutes_str)
-            hours = int(match.group("hours"))
-            minutes = int(match.group("minutes"))
-            if minutes > 59:
-                raise ValueError("invalid minutes value: %s" % hours_minutes_str)
-            return hours * 60 + minutes
-
-        def parse_quantity(quantity_str: str) -> int:
-            match = re.search(r"^(?P<int>[0-9]+) (?P<unit>szt\.)", quantity_str)
-            if not match:
-                raise ValueError("invalid quantity: %s" % quantity_str)
-            return int(match.group("int"))
-
-        def parse_float(re_match: Match) -> float:
-            value = float(re_match.group("int"))
-            if re_match.group("fract") is not None:
-                value += float("." + re_match.group("fract"))
-            return value
-
-        row_xpath = (
-            "//div[contains(@class, 'border-apla')]"
-            "/div[@class='level']"
-        )
-        label_xpath = "./div[contains(@class, 'level-left')]"
-        value_xpath = "./div[contains(@class, 'level-item')]"
-        parsed = self.parse_table(html_code, row_xpath, label_xpath, value_xpath, False)
-        label_map = {
-            'Konto': 'balance_PLN',
-            'Data wa\u017cno\u015bci po\u0142\u0105cze\u0144 wychodz\u0105cych':
-                'outgoing_expiration_date',
-            'Data wa\u017cno\u015bci po\u0142\u0105cze\u0144 przychodz\u0105cych':
-                'incoming_expiration_date',
-            'Promocyjny Internet': 'data_sale',
-            'Liczba promocyjnych GB': 'free_data_GB',
-            'Limit GB w roamingu UE': 'cheaper_roaming_EU_data_GB',
-            'Limit wydatk\xf3w na us\u0142ugi Premium': 'premium_services_limit_PLN',
-            'Suma do\u0142adowa\u0144 w tym miesi\u0105cu': 'credit_this_month_PLN',
-            'Minuty na Ukrain\u0119': 'UA_minutes',
-            'Minuty do wszystkich sieci': 'minutes_all_networks',
-            'SMS-y do wszystkich': 'SMS_all_count',
-            '100 GB na lato': 'summer_data_100_GB',
-        }
-        value_parsers = {
-            'balance_PLN': parse_balance,
-            'outgoing_expiration_date': parse_date,
-            'incoming_expiration_date': parse_date,
-            'data_sale': lambda x: x,
-            'free_data_GB': parse_data_cap,
-            'cheaper_roaming_EU_data_GB': parse_data_cap,
-            'premium_services_limit_PLN': parse_balance,
-            'credit_this_month_PLN': parse_balance,
-            'UA_minutes': parse_hours_minutes,
-            'minutes_all_networks': parse_hours_minutes,
-            'SMS_all_count': parse_quantity,
-            'summer_data_100_GB': parse_data_cap,
-        }
-        return {
-            label_map[label]: value_parsers[label_map[label]](value)
-            for label, value in parsed.items()
-        }
-
-    def parse_services_data(self, html_code: str) -> Mapping[str, bool]:
-        row_xpath = "//div[contains(@class, 'image-tile')]"
-        label_xpath = ".//p[contains(@class, 'temp_title')]"
-        value_xpath = ".//div[contains(@class, 'active-label')]"
-        flag_xpath = ".//div[contains(@class, 'tile-actions')]/div[contains(., 'miesi\u0119cznie')]"
-        label_map = {
-            ('Noce bez limitu', False): 'no_data_limit_nights',
-            ('Noce bez limitu', True): 'no_data_limit_nights_recurring',
-            ('Dzie\u0144 bez limitu w Play Internet na Kart\u0119', False): 'no_data_limit_day',
-            ('Tydzie\u0144 bez limitu GB', False): 'no_data_limit_week',
-            ('Miesi\u0105c bez limitu GB', False): 'no_data_limit_month',
-            ('Miesi\u0105c bez limitu GB', True): 'no_data_limit_month_recurring',
-            ('Ta\u0144sze po\u0142\u0105czenia i smsy na Ukrain\u0119', True): 'cheaper_UA',
-            ('1000 minut na Ukrain\u0119', False): 'voice_bundle_1000min_UA',
-            ('1000 minut na Ukrain\u0119 + 10 GB na Viber', False):
-                'voice_bundle_1000min_UA_Viber_10GB',
-            ('Roaming zagraniczny', False): 'roaming',
-            ('500 MB do wykorzystania w UE', False): 'roaming_EU_data_bundle_500MB',
-            ('1 GB do wykorzystania w UE', False): 'roaming_EU_data_bundle_1GB',
-            ('3 GB do wykorzystania w UE', False): 'roaming_EU_data_bundle_3GB',
-            ('Pakiet Internet Emiraty 150 MB', False): 'roaming_AE_data_bundle_150MB',
-            ('Pakiet Internet \u015awiat 1 GB', False): 'roaming_data_bundle_1GB',
-            ('Pakiet Internet \u015awiat 300 MB', False): 'roaming_data_bundle_300MB',
-            ('Nawet 200 GB za darmo dla student\xf3w', False): 'free_data_200GB_for_students',
-            ('29 gr za minut\u0119 do Bangladeszu', False): 'voice_29_BD',
-            ('29 gr za minut\u0119 do Indii', False): 'voice_29_IN',
-            ('70 gr za minut\u0119 do Nepalu', False): 'voice_29_NP',
-            ('Taniej do Bangladeszu', False): 'cheaper_BD',
-            ('Taniej do Indii', False): 'cheaper_IN',
-            ('Taniej do Nepalu', False): 'cheaper_NP',
-        }
-        value_map = {
-            '': False,
-            'W\u0142\u0105czony': True,
-        }
-        parsed = self.parse_flagged_table(
-            html_code,
-            row_xpath,
-            label_xpath,
-            value_xpath,
-            flag_xpath
-        )
-        return {label_map[label]: value_map[value] for label, value in parsed.items()}
-
-    @staticmethod
-    def parse_table(
-            html_code: str,
-            row_xpath: str,
-            label_xpath: str,
-            value_xpath: str,
-            allow_empty_value: bool
-    ) -> Mapping[str, str]:
-        return {
-            xpath_text(row_node, label_xpath, False):
-            first_line(xpath_text(row_node, value_xpath, allow_empty_value)).strip()
-            for row_node in html.fromstring(html_code).xpath(row_xpath)
-        }
-
-    @staticmethod
-    def parse_flagged_table(
-            html_code: str,
-            row_xpath: str,
-            label_xpath: str,
-            value_xpath: str,
-            flag_xpath: str
-    ) -> Mapping[Tuple[str, bool], str]:
-        return {
-            (xpath_text(row_node, label_xpath, True), bool(row_node.xpath(flag_xpath))):
-            first_line(xpath_text(row_node, value_xpath, True)).strip()
-            for row_node in html.fromstring(html_code).xpath(row_xpath)
-        }
 
     def follow_js_form_redirection(self, response: requests.Response) -> requests.Response:
         form = html.fromstring(response.content).xpath("//form[1]")[0]
@@ -384,6 +228,165 @@ class DWRServices(DWRMethod):
     @staticmethod
     def parse_response(response_body: str) -> str:
         return DWRBalance.parse_response(response_body)
+
+
+class Parser:
+
+    def parse_balance_data(self, html_code: str) -> Mapping[str, BalanceValue]:
+
+        def parse_balance(balance_str: str) -> float:
+            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]{2})){0,1} z\u0142", balance_str)
+            if not match:
+                raise ValueError("invalid balance: %s" % balance_str)
+            return parse_float(match)
+
+        def parse_date(date_str: str) -> datetime.date:
+            return datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
+
+        def parse_data_cap(cap_str: str) -> float:
+            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]+)){0,1} (?P<unit>GB|MB)", cap_str)
+            if not match:
+                raise ValueError("invalid data cap: %s" % cap_str)
+            value = parse_float(match)
+            if match.group("unit") == "MB":
+                value /= 1000
+            return value
+
+        def parse_hours_minutes(hours_minutes_str: str) -> int:
+            match = re.search("^(?P<hours>[0-9]+):(?P<minutes>[0-9]+) min", hours_minutes_str)
+            if not match:
+                raise ValueError("invalid hours:minutes value: %s" % hours_minutes_str)
+            hours = int(match.group("hours"))
+            minutes = int(match.group("minutes"))
+            if minutes > 59:
+                raise ValueError("invalid minutes value: %s" % hours_minutes_str)
+            return hours * 60 + minutes
+
+        def parse_quantity(quantity_str: str) -> int:
+            match = re.search(r"^(?P<int>[0-9]+) (?P<unit>szt\.)", quantity_str)
+            if not match:
+                raise ValueError("invalid quantity: %s" % quantity_str)
+            return int(match.group("int"))
+
+        def parse_float(re_match: Match) -> float:
+            value = float(re_match.group("int"))
+            if re_match.group("fract") is not None:
+                value += float("." + re_match.group("fract"))
+            return value
+
+        row_xpath = (
+            "//div[contains(@class, 'border-apla')]"
+            "/div[@class='level']"
+        )
+        label_xpath = "./div[contains(@class, 'level-left')]"
+        value_xpath = "./div[contains(@class, 'level-item')]"
+        parsed = self.parse_table(html_code, row_xpath, label_xpath, value_xpath, False)
+        label_map = {
+            'Konto': 'balance_PLN',
+            'Data wa\u017cno\u015bci po\u0142\u0105cze\u0144 wychodz\u0105cych':
+                'outgoing_expiration_date',
+            'Data wa\u017cno\u015bci po\u0142\u0105cze\u0144 przychodz\u0105cych':
+                'incoming_expiration_date',
+            'Promocyjny Internet': 'data_sale',
+            'Liczba promocyjnych GB': 'free_data_GB',
+            'Limit GB w roamingu UE': 'cheaper_roaming_EU_data_GB',
+            'Limit wydatk\xf3w na us\u0142ugi Premium': 'premium_services_limit_PLN',
+            'Suma do\u0142adowa\u0144 w tym miesi\u0105cu': 'credit_this_month_PLN',
+            'Minuty na Ukrain\u0119': 'UA_minutes',
+            'Minuty do wszystkich sieci': 'minutes_all_networks',
+            'SMS-y do wszystkich': 'SMS_all_count',
+            '100 GB na lato': 'summer_data_100_GB',
+        }
+        value_parsers = {
+            'balance_PLN': parse_balance,
+            'outgoing_expiration_date': parse_date,
+            'incoming_expiration_date': parse_date,
+            'data_sale': lambda x: x,
+            'free_data_GB': parse_data_cap,
+            'cheaper_roaming_EU_data_GB': parse_data_cap,
+            'premium_services_limit_PLN': parse_balance,
+            'credit_this_month_PLN': parse_balance,
+            'UA_minutes': parse_hours_minutes,
+            'minutes_all_networks': parse_hours_minutes,
+            'SMS_all_count': parse_quantity,
+            'summer_data_100_GB': parse_data_cap,
+        }
+        return {
+            label_map[label]: value_parsers[label_map[label]](value)
+            for label, value in parsed.items()
+        }
+
+    def parse_services_data(self, html_code: str) -> Mapping[str, bool]:
+        row_xpath = "//div[contains(@class, 'image-tile')]"
+        label_xpath = ".//p[contains(@class, 'temp_title')]"
+        value_xpath = ".//div[contains(@class, 'active-label')]"
+        flag_xpath = ".//div[contains(@class, 'tile-actions')]/div[contains(., 'miesi\u0119cznie')]"
+        label_map = {
+            ('Noce bez limitu', False): 'no_data_limit_nights',
+            ('Noce bez limitu', True): 'no_data_limit_nights_recurring',
+            ('Dzie\u0144 bez limitu w Play Internet na Kart\u0119', False): 'no_data_limit_day',
+            ('Tydzie\u0144 bez limitu GB', False): 'no_data_limit_week',
+            ('Miesi\u0105c bez limitu GB', False): 'no_data_limit_month',
+            ('Miesi\u0105c bez limitu GB', True): 'no_data_limit_month_recurring',
+            ('Ta\u0144sze po\u0142\u0105czenia i smsy na Ukrain\u0119', True): 'cheaper_UA',
+            ('1000 minut na Ukrain\u0119', False): 'voice_bundle_1000min_UA',
+            ('1000 minut na Ukrain\u0119 + 10 GB na Viber', False):
+                'voice_bundle_1000min_UA_Viber_10GB',
+            ('Roaming zagraniczny', False): 'roaming',
+            ('500 MB do wykorzystania w UE', False): 'roaming_EU_data_bundle_500MB',
+            ('1 GB do wykorzystania w UE', False): 'roaming_EU_data_bundle_1GB',
+            ('3 GB do wykorzystania w UE', False): 'roaming_EU_data_bundle_3GB',
+            ('Pakiet Internet Emiraty 150 MB', False): 'roaming_AE_data_bundle_150MB',
+            ('Pakiet Internet \u015awiat 1 GB', False): 'roaming_data_bundle_1GB',
+            ('Pakiet Internet \u015awiat 300 MB', False): 'roaming_data_bundle_300MB',
+            ('Nawet 200 GB za darmo dla student\xf3w', False): 'free_data_200GB_for_students',
+            ('29 gr za minut\u0119 do Bangladeszu', False): 'voice_29_BD',
+            ('29 gr za minut\u0119 do Indii', False): 'voice_29_IN',
+            ('70 gr za minut\u0119 do Nepalu', False): 'voice_29_NP',
+            ('Taniej do Bangladeszu', False): 'cheaper_BD',
+            ('Taniej do Indii', False): 'cheaper_IN',
+            ('Taniej do Nepalu', False): 'cheaper_NP',
+        }
+        value_map = {
+            '': False,
+            'W\u0142\u0105czony': True,
+        }
+        parsed = self.parse_flagged_table(
+            html_code,
+            row_xpath,
+            label_xpath,
+            value_xpath,
+            flag_xpath
+        )
+        return {label_map[label]: value_map[value] for label, value in parsed.items()}
+
+    @staticmethod
+    def parse_table(
+            html_code: str,
+            row_xpath: str,
+            label_xpath: str,
+            value_xpath: str,
+            allow_empty_value: bool
+    ) -> Mapping[str, str]:
+        return {
+            xpath_text(row_node, label_xpath, False):
+            first_line(xpath_text(row_node, value_xpath, allow_empty_value)).strip()
+            for row_node in html.fromstring(html_code).xpath(row_xpath)
+        }
+
+    @staticmethod
+    def parse_flagged_table(
+            html_code: str,
+            row_xpath: str,
+            label_xpath: str,
+            value_xpath: str,
+            flag_xpath: str
+    ) -> Mapping[Tuple[str, bool], str]:
+        return {
+            (xpath_text(row_node, label_xpath, True), bool(row_node.xpath(flag_xpath))):
+            first_line(xpath_text(row_node, value_xpath, True)).strip()
+            for row_node in html.fromstring(html_code).xpath(row_xpath)
+        }
 
 
 def xpath_text(parent_node: html.HtmlElement, xpath: str, allow_empty: bool) -> str:
