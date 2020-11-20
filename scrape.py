e93c0cb1f391b35deeb8a869004930ed1e6b48ef
Paweł Słowik <pawel.slowik@zoho.com>
2020-11-20 14:25:59 +0100

    Move parser utility functions to module level

--- scrape.py
+++ scrape.py
@@ -234,47 +234,6 @@ class Parser:
 
     @classmethod
     def parse_balance_data(cls, html_code: str) -> Mapping[str, BalanceValue]:
-
-        def parse_balance(balance_str: str) -> float:
-            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]{2})){0,1} z\u0142", balance_str)
-            if not match:
-                raise ValueError("invalid balance: %s" % balance_str)
-            return parse_float(match)
-
-        def parse_date(date_str: str) -> datetime.date:
-            return datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
-
-        def parse_data_cap(cap_str: str) -> float:
-            match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]+)){0,1} (?P<unit>GB|MB)", cap_str)
-            if not match:
-                raise ValueError("invalid data cap: %s" % cap_str)
-            value = parse_float(match)
-            if match.group("unit") == "MB":
-                value /= 1000
-            return value
-
-        def parse_hours_minutes(hours_minutes_str: str) -> int:
-            match = re.search("^(?P<hours>[0-9]+):(?P<minutes>[0-9]+) min", hours_minutes_str)
-            if not match:
-                raise ValueError("invalid hours:minutes value: %s" % hours_minutes_str)
-            hours = int(match.group("hours"))
-            minutes = int(match.group("minutes"))
-            if minutes > 59:
-                raise ValueError("invalid minutes value: %s" % hours_minutes_str)
-            return hours * 60 + minutes
-
-        def parse_quantity(quantity_str: str) -> int:
-            match = re.search(r"^(?P<int>[0-9]+) (?P<unit>szt\.)", quantity_str)
-            if not match:
-                raise ValueError("invalid quantity: %s" % quantity_str)
-            return int(match.group("int"))
-
-        def parse_float(re_match: Match) -> float:
-            value = float(re_match.group("int"))
-            if re_match.group("fract") is not None:
-                value += float("." + re_match.group("fract"))
-            return value
-
         row_xpath = (
             "//div[contains(@class, 'border-apla')]"
             "/div[@class='level']"
@@ -389,6 +348,52 @@ class Parser:
             first_line(xpath_text(row_node, value_xpath, True)).strip()
             for row_node in html.fromstring(html_code).xpath(row_xpath)
         }
+
+
+def parse_balance(balance_str: str) -> float:
+    match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]{2})){0,1} z\u0142", balance_str)
+    if not match:
+        raise ValueError("invalid balance: %s" % balance_str)
+    return parse_float(match)
+
+
+def parse_date(date_str: str) -> datetime.date:
+    return datetime.datetime.strptime(date_str, "%d.%m.%Y").date()
+
+
+def parse_data_cap(cap_str: str) -> float:
+    match = re.search("^(?P<int>[0-9]+)(,(?P<fract>[0-9]+)){0,1} (?P<unit>GB|MB)", cap_str)
+    if not match:
+        raise ValueError("invalid data cap: %s" % cap_str)
+    value = parse_float(match)
+    if match.group("unit") == "MB":
+        value /= 1000
+    return value
+
+
+def parse_hours_minutes(hours_minutes_str: str) -> int:
+    match = re.search("^(?P<hours>[0-9]+):(?P<minutes>[0-9]+) min", hours_minutes_str)
+    if not match:
+        raise ValueError("invalid hours:minutes value: %s" % hours_minutes_str)
+    hours = int(match.group("hours"))
+    minutes = int(match.group("minutes"))
+    if minutes > 59:
+        raise ValueError("invalid minutes value: %s" % hours_minutes_str)
+    return hours * 60 + minutes
+
+
+def parse_quantity(quantity_str: str) -> int:
+    match = re.search(r"^(?P<int>[0-9]+) (?P<unit>szt\.)", quantity_str)
+    if not match:
+        raise ValueError("invalid quantity: %s" % quantity_str)
+    return int(match.group("int"))
+
+
+def parse_float(re_match: Match) -> float:
+    value = float(re_match.group("int"))
+    if re_match.group("fract") is not None:
+        value += float("." + re_match.group("fract"))
+    return value
 
 
 def xpath_text(parent_node: html.HtmlElement, xpath: str, allow_empty: bool) -> str:
